2021f3d25602f2cc91e01b5dc8d7779b9f376bff
SusanQuintanilla16 <susan.quintanilla@gmail.com>
2018-03-14 00:11:02 -0600

    Proyecto 1
    Primera pruebas del uso de GitHub

---
+++ JavaApp/src/udb/edu/sv/Java1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udb.edu.sv;
+
+/**
+ *
+ * @author Susan
+ */
+public class Java1 {
+    public static void main(String[] args) {
+        System.out.println("Esta es una prueba del uso de repositorios"
+    }
+}
