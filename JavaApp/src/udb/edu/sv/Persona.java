e932abe756e7bc536670957774a8a63953e4263b
SusanQuintanilla16 <susan.quintanilla@gmail.com>
2018-03-14 00:31:29 -0600

    Clase persona

---
+++ JavaApp/src/udb/edu/sv/Persona.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udb.edu.sv;
+
+/**
+ *
+ * @author Susan
+ */
+public class Persona {
+    String Nombre;
+    String Apellido;
+    static int Cantidad=0;
+    
+    public Persona()
+    {
+        Cantidad++;
+    }
+}
